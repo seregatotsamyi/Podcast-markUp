b373da6b22a9d186b1fc513d88a547e18819584b
seregatotsamyi <iserg.mos.ls@gmail.com>
2023-07-26 14:28:28 +0500

    skeleton

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,20 @@ window.onload = function () {
 };
 
 
-$(function(){
+$(function () {
   let width = $(window).width();
   let body = $('.body');
+
+  $(document).on('click', '.js-auth-list', function (e) {
+    $('.header__auth-bottom').toggleClass('_open')
+  })
+  $(document).on('click', '.js-toggle-menu', function (e) {
+    $('.menu').toggleClass('_open')
+  })
+  if (width < 769){
+    $(document).on('click', '.search__btn-open', function (e) {
+      $('.search').toggleClass('_open')
+    })
+  }
+  
 });
